5388b951f6aa81bff4f95c42cadc0fa579163885
Srijith <srijith@kali.kali>
2024-03-02 22:52:00 +0530

    clearing,Writing to expression --nobugs

--- script.js
+++ script.js
@@ -1,2 +1,58 @@
-const inputBox = document.getElementById("input");
-const expression = document.getElementById('c')
+document.addEventListener('DOMContentLoaded', function() {
+
+const inputBox = document.getElementById('input');
+let expression = '';
+let result = '';
+
+function actionRecv(id,action){
+    if((expression.endsWith('÷') || expression.endsWith('%') || expression.endsWith('*') || expression.endsWith('-') || expression.endsWith('+')) && action == 'op' ){
+        expression= expression.slice(0, -1) + id;
+    }else{
+        expression += id;
+    }
+}
+function clear(){
+    expression = 'clearing';
+    setTimeout(() => {
+        expression = 'clearing.';
+        updateDisp(expression,'')
+        setTimeout(() => {
+            expression = 'clearing..';
+            updateDisp(expression,'')
+            setTimeout(() => {
+                expression = 'clearing...';
+                updateDisp(expression,'')
+                setTimeout(() => {
+                    expression='';
+                    updateDisp(expression,'')
+                }, 250);                
+            }, 200);
+        }, 150);
+    }, 100);
+    
+}
+
+function buttonClick(event){
+    const action = event.target.dataset.use;
+    const id = event.target.dataset.id
+
+    switch(action){
+        case 'number':
+        case 'op':
+        actionRecv(id,action)
+        break;
+        case 'clear':
+        clear()
+        break;
+    }
+    updateDisp(expression,result)
+}
+
+inputBox.addEventListener('click',buttonClick)
+
+});
+
+function updateDisp(expression,result){
+    document.getElementById('expression').textContent = expression.replace(/\*/g,'x').replace(/\//g,'÷');
+    document.getElementById('result').textContent=result;
+}
